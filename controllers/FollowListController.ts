f492b7cbb0c7d713e3485b13b79428449762ed3e
taasha10 <sharmapratyasha1@gmail.com>
2022-12-14 17:49:21 -0500

    implemented followListComtroller

--- controllers/FollowListController.ts
+++ controllers/FollowListController.ts
@@ -25,27 +25,51 @@ export default class FollowListController implements FollowListControllerI{
             app.get("/user/:uid/followers/", FollowListController.followListController.findAllFollowers);
             app.get("/user/:uid/following/", FollowListController.followListController.findAllFollowing);
             app.post("/follow/add/:uid",  FollowListController.followListController.addFollowing);
-            app.get("/user/find/following/:uid", FollowListController.followListController.findFollowingByUserId)
-            app.get("/user/find/follower/:uid", FollowListController.followListController.findFollowersByUserId);
             app.delete("/follow/delete/:uid", FollowListController.followListController.deleteFollowing);
 
         }
         return FollowListController.followListController;
     }
 
+    /**
+     * Responsible for adding a user to follow.
+     * @param req  request containing the userId to find the user.
+     * @param res response that is to be sent to the user.
+     */
     addFollowing = async (req: Request, res: Response) => {
+        const uid = req.params.uid;
+        const status = await FollowListController.followListDao.addFollowing(uid);
+        res.json(status);
 
     }
 
+    /**
+     * Responsible for deleting a user using their id from the following list.
+     * @param req  request containing the userId to find the user.
+     * @param res response that is to be sent to the user.
+     */
     deleteFollowing = async (req: Request, res: Response) => {
+        const uid = req.params.uid;
+        const status = await FollowListController.followListDao.deleteFollowing(uid);
+        res.json(status);
     }
 
+    /**
+     * Responsible for finding all users in followers list using a user id.
+     * @param req  request containing the userId to find the user.
+     * @param res response that is to be sent to the user.
+     */
     findAllFollowers = async (req: Request, res: Response) => {
         const uid = req.params.uid;
         const result = await FollowListController.followListDao.findAllFollowers(uid);
         res.json(result);
     }
 
+    /**
+     * Responsible for finding all users in following list using a user id.
+     * @param req  request containing the userId to find the user.
+     * @param res response that is to be sent to the user.
+     */
     findAllFollowing = async (req: Request, res: Response) => {
         const uid = req.params.uid;
         const result = await FollowListController.followListDao.findAllFollowing(uid);
