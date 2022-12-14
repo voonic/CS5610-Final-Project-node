3e23db1b2645598dfb66c0b57781b5ca82bfc277
taasha10 <sharmapratyasha1@gmail.com>
2022-12-14 04:25:46 -0500

    updated getInstance() of UserController

--- controllers/FollowListController.ts
+++ controllers/FollowListController.ts
@@ -1,7 +1,33 @@
 import FollowListControllerI from "../interfaces/FollowListControllerI";
-import {Request, Response} from "express";
+import {Express, Request, Response} from "express";
+import UserDao from "../dao/UserDao";
 
 export default class FollowListController implements FollowListControllerI{
+
+    private static userDao: UserDao = new UserDao();
+    private static FollowListController: FollowListController | null = null;
+
+    /**
+     * Private constructor because we want singleton pattern to be used
+     * for creating object of this class.
+     */
+    private constructor() { }
+
+    /**
+     * Creates singleton controller instance
+     * @param {Express} app Express instance to declare the RESTful Web service
+     * API
+     * @return AuthController
+     */
+    public static getInstance = (app: Express): FollowListController => {
+        if (FollowListController.followListController === null) {
+            FollowListController.followListController = new FollowListController();
+            app.get("/user/:uid", FollowListController.followListController.findUserById);
+            app.put("/user/:uid",  FollowListController.followListController.updateUser);
+        }
+        return FollowListController.followListController;
+    }
+
     addFollowing = async (req: Request, res: Response) => {
 
     }
