7e5bc2abe16b2b19175058eb0568981d7de0af09
taasha10 <sharmapratyasha1@gmail.com>
2022-12-14 04:09:20 -0500

    added comments in userController

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -31,12 +31,22 @@ export default class UserController implements UserControllerI {
         return UserController.userController;
     }
 
+    /**
+     * Responsible for finding a user using their id.
+     * @param req  request containing the userId to find the user.
+     * @param res response that is to be sent to the user.
+     */
     findUserById = async (req: Request, res: Response) => {
         const uId = req.params.uid;
         const result = await UserController.userDao.findUserById(uId);
         res.json(result);
     }
 
+    /**
+     * Responsible for updating an existing user using their id.
+     * @param req  request containing the userId to find the user.
+     * @param res response that is to be sent to the user after updating the user.
+     */
     updateUser = async (req: Request, res: Response) => {
         const uid = req.params.uid;
         const user = req.body;
@@ -44,29 +54,54 @@ export default class UserController implements UserControllerI {
         res.json(result);
     }
 
+    /**
+     * Responsible for creating a user in the database.
+     * @param req  request containing the user details in the body.
+     * @param res response that is to be sent as a new user.
+     */
     createUser = async (req: Request, res: Response) => {
         const user = req.body;
         const result = await UserController.userDao.createUser(user);
         res.json(result);
     }
 
+    /**
+     * Responsible for deleting an existing user using their id.
+     * @param req  request containing the userId to find the user.
+     * @param res response that is to be sent to the user post deletion.
+     */
     deleteUser = async (req: Request, res: Response) => {
         const uid = req.params.uid;
         const result = await UserController.userDao.deleteUser(uid);
         res.json(result);
     }
 
+    /**
+     * Responsible for deleting a user using their email.
+     * @param req  request containing the user email to find the user.
+     * @param res response that is to be sent to the user post deletion.
+     */
     deleteUserByEmail = async (req: Request, res: Response) => {
         const email = req.params.email;
         const result = await UserController.userDao.deleteUser(email);
         res.json(result);
     }
 
+    /**
+     * Responsible for finding all users in the database.
+     * @param req  request.
+     * @param res response that is to be sent to the user.
+     */
     findAllUsers = async (req: Request, res: Response) => {
         const result = await UserController.userDao.findAllUsers();
         res.json(result);
     }
 
+    /**
+     * Responsible for finding a user using their email.
+     * @param req  request containing the user email to find the user.
+     * @param res response that is to be sent to the user.
+     */
     findUserByEmail = async (req: Request, res: Response) => {
         const email = req.params.email;
         const result = await UserController.userDao.findUserById(email);
